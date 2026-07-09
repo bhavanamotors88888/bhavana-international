a43ac6b6dbdb850fba0ff4fb1bea1d08313ecfd9
kavya patel <181580683+HK1152@users.noreply.github.com>
2026-07-09 17:44:41 +0530

    done

--- backend/src/app/modules/contact/contact.controller.js
+++ backend/src/app/modules/contact/contact.controller.js
@@ -1,4 +1,4 @@
-import { processContactInquiry, testSMTPConnection } from './contact.service.js';
+import { processContactInquiry } from './contact.service.js';
 import { sendSuccess } from '../../utils/response.util.js';
 
 export const handleContactSubmit = async (req, res, next) => {
@@ -9,16 +9,6 @@ export const handleContactSubmit = async (req, res, next) => {
     const result = await processContactInquiry(contactData);
 
     return sendSuccess(res, 200, 'Your message has been sent successfully. We will contact you soon.', result);
-  } catch (error) {
-    next(error);
-  }
-};
-
-
-export const handleSMTPTest = async (req, res, next) => {
-  try {
-    const result = await testSMTPConnection();
-    return res.status(result.success ? 200 : 500).json(result);
   } catch (error) {
     next(error);
   }
